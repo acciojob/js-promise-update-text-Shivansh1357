973a278648e20665102a86f75e2291a5ef56aff1
Shivansh1357 <120310888+Shivansh1357@users.noreply.github.com>
2023-06-05 10:59:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function updateElementTextWithPromise() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+const outputElement = document.getElementById("output");
+
+updateElementTextWithPromise()
+  .then((text) => {
+    outputElement.textContent = text;
+  })
+  .catch((error) => {
+    console.error("An error occurred:", error);
+  });
